0b31a95e627d31f6a1d5432bdd36481c04da52b6
duckie <sarthakmisraa@gmail.com>
2020-11-21 21:11:07 +0530

    added the switch statment

---
+++ switch/switch.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main(){
+	var n int
+	fmt.Print("Enter a number >")
+	tmp := 1
+	for tmp == 1 {
+		tmp, _ = fmt.Scanf("%d",&n)
+		switch n {
+			case 1 : fmt.Println("Monday")
+			case 2 : fmt.Println("Tuesday")
+			case 3 : fmt.Println("Wednesday")
+			case 4 : fmt.Println("Thrusday")
+			case 5 : fmt.Println("Friday")
+			case 6 : fmt.Println("Saturday")
+			case 7 : fmt.Println("Sunday")
+			default: fmt.Println("Input range from 1-7")
+		}
+	}
+}
