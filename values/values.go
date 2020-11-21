ada15f0efce59c31a6c311a907c14f9487459ce3
duckie <sarthakmisraa@gmail.com>
2020-11-21 14:32:12 +0530

    added the value types code

---
+++ values/values.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+"fmt"
+)
+
+
+func main(){
+	fmt.Println("go " + "lang")
+	fmt.Println("60 + 9 = ",60 + 9)
+	fmt.Println("6.0 + 0.9 = ",6.0 + 0.9)
+	fmt.Println(true && false)
+	fmt.Println(true || false)
+	fmt.Println(!true)
+}
