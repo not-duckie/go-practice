53521f198f50b3f4b409ae940b8176b15e85a531
duckie <sarthakmisraa@gmail.com>
2020-12-18 14:27:47 +0530

    added a numguess game

---
+++ conditionals/numguess.go
@@ -0,0 +1,26 @@
+package main
+
+import ("fmt"
+	"math/rand"
+	"time"
+	"os")
+
+func main(){
+	src := rand.NewSource(time.Now().UnixNano())
+	r1 := rand.New(src)
+	num := r1.Intn(100)
+	if (num<=50){
+		fmt.Println("Hint#1 The number is less than or equal to 50")
+	} else { fmt.Println("Hint#1 The number is greater than 50") }
+	n := 0
+	for n != num {
+		fmt.Print(">")
+		fmt.Scanf("%d",&n)
+		if (n>num){
+			fmt.Println("Your guess is too big")
+		} else if (n<num){ fmt.Println("Your guess it too small")
+		} else { fmt.Println("You guessed it right !!")
+			 os.Exit(0)
+			}
+	}
+}
