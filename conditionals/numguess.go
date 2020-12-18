cb4a6a79ddff6e52784c7e760ce9df9daf30d7b3
duckie <sarthakmisraa@gmail.com>
2020-12-18 14:31:51 +0530

    added a score feature

--- conditionals/numguess.go
+++ conditionals/numguess.go
@@ -13,13 +13,16 @@ func main(){
 		fmt.Println("Hint#1 The number is less than or equal to 50")
 	} else { fmt.Println("Hint#1 The number is greater than 50") }
 	n := 0
+	tmp := 0
 	for n != num {
+		tmp++
 		fmt.Print(">")
 		fmt.Scanf("%d",&n)
 		if (n>num){
 			fmt.Println("Your guess is too big")
 		} else if (n<num){ fmt.Println("Your guess it too small")
 		} else { fmt.Println("You guessed it right !!")
+			 fmt.Println("You guess it in ",tmp," tries")
 			 os.Exit(0)
 			}
 	}
