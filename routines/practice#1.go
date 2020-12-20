35b87f6ebeb40d426c27ae87330dbfd5240b34e9
duckie <sarthakmisraa@gmail.com>
2020-12-20 21:08:08 +0530

    got it working

--- routines/practice#1.go
+++ routines/practice#1.go
@@ -3,7 +3,21 @@ package main
 import ("fmt"
 		"sync")
 
+var wg = sync.WaitGroup{}
+var m = sync.RWMutex{}
+
+func printHello(i int){
+	fmt.Printf("fuckme %d\n",i)
+	m.Unlock()
+	wg.Done()
+}
+
 func main(){
-	
+	for i:=0;i<10;i++{
+		wg.Add(1)
+		m.Lock()
+		go printHello(i)
+	}
+	wg.Wait()
 }
 
