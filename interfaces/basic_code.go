43f3693aee55b2d7f6b53294744428b52f7baf44
duckie <sarthakmisraa@gmail.com>
2020-12-20 21:08:34 +0530

    UwU

--- interfaces/basic_code.go
+++ interfaces/basic_code.go
@@ -2,6 +2,20 @@ package main
 
 import "fmt"
 
+type ConsoleWriter struct {}
+
+type fuck struct {}
+
+type writer interface{
+	Write([]byte) (int ,error)
+}
+
+func (cw ConsoleWriter) Write(data []byte)(int,error){
+	n,err := fmt.Println(string(data))
+	return n,err
+}
+
 func main(){
-	
+	var w ConsoleWriter = ConsoleWriter{}
+	w.Write([]byte("hello bitch"))
 }
